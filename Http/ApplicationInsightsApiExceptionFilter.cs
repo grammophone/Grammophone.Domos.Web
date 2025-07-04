8e837854186972be1fd5c748794cc405a9f04345
grammophone <emmanuel.liossis@gmail.com>
2025-07-03 21:48:00 -0400

    Update the Application insights filters to generate 'trace' events whenever they encounter user exceptions not to be tracked as 'exceptions'.

--- Http/ApplicationInsightsApiExceptionFilter.cs
+++ Http/ApplicationInsightsApiExceptionFilter.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Web.Http.Filters;
 using Grammophone.DataAccess;
 using Grammophone.Domos.Logic;
+using Grammophone.Domos.Web.Mvc;
 
 namespace Grammophone.Domos.Web.Http
 {
@@ -24,13 +25,29 @@ namespace Grammophone.Domos.Web.Http
 
 			switch (exception)
 			{
+				case ActionException actionException:
+					var actionTelemetry = new Microsoft.ApplicationInsights.TelemetryClient();
+
+					actionTelemetry.TrackTrace(actionException.Message, Microsoft.ApplicationInsights.DataContracts.SeverityLevel.Warning);
+
+					break;
+
 				//case AccessDeniedException accessDeniedException:
 				case IntegrityViolationException integrityConstraintExceptinon:
+					var integrityConstraintMessageTelemetry = new Microsoft.ApplicationInsights.TelemetryClient();
+
+					integrityConstraintMessageTelemetry.TrackTrace(integrityConstraintExceptinon.Message, Microsoft.ApplicationInsights.DataContracts.SeverityLevel.Information);
+					break;
+
 				case UserException userException:
-				//case InvalidOperationException invalidOperationException:
+					var userMessageTelemetry = new Microsoft.ApplicationInsights.TelemetryClient();
+
+					userMessageTelemetry.TrackTrace(userException.Message, Microsoft.ApplicationInsights.DataContracts.SeverityLevel.Information);
+					//case InvalidOperationException invalidOperationException:
 					// These exceptions are intended to communicate errors to the user,
 					// thus do not record them as system errors.
 					break;
+
 
 				default:
 					var telemetry = new Microsoft.ApplicationInsights.TelemetryClient();
